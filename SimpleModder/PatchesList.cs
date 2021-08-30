725ba46cac92f0c1e5c7ecadb54c33ac48f106b4
Kazurin Nanako <71819243+Kazurin-775@users.noreply.github.com>
2021-08-30 14:52:25 +0800

    Add patches scanner

---
+++ SimpleModder/PatchesList.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SimpleModder
+{
+    public static class PatchesList
+    {
+        public static List<string> Obtain()
+        {
+            if (!Directory.Exists("patches"))
+                return new List<string>();
+            return Directory.EnumerateFiles("patches")
+                .Where(File.Exists)
+                .Select(Path.GetFileName)
+                .ToList();
+        }
+    }
+}
--- SimpleModdings/MainWindow.xaml.cs
+++ SimpleModdings/MainWindow.xaml.cs
@@ -1,10 +1,26 @@
-﻿namespace SimpleModdings
+﻿using System.Collections.Generic;
+using ModernWpf.Controls;
+using SimpleModder;
+
+namespace SimpleModdings
 {
     public partial class MainWindow
     {
+        private List<string> _patchesList;
+
         public MainWindow()
         {
             InitializeComponent();
+            _patchesList = PatchesList.Obtain();
+            if (_patchesList.Count == 0)
+            {
+                new ContentDialog
+                {
+                    Title = "找不到补丁",
+                    Content = "我们没有在 patches 目录中发现任何补丁。\n请检查程序的配置是否正确。",
+                    CloseButtonText = "确定",
+                }.ShowAsync();
+            }
         }
     }
 }
