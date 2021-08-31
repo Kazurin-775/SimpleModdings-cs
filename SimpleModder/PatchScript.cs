fc1fadae627bf5a23b9be09b34956fefc06331c4
Kazurin Nanako <71819243+Kazurin-775@users.noreply.github.com>
2021-09-01 01:23:15 +0800

    Create `PatchedFile`

--- SimpleModder/PatchScript.cs
+++ SimpleModder/PatchScript.cs
@@ -1,9 +1,5 @@
 ﻿using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
-using SimpleModder.Patches;
 
 namespace SimpleModder
 {
@@ -13,7 +9,7 @@ namespace SimpleModder
         public readonly string DefaultPath;
         public readonly string Comments;
 
-        private readonly Dictionary<string, List<Patch>> _patches = new Dictionary<string, List<Patch>>();
+        private readonly Dictionary<string, PatchedFile> _patches = new Dictionary<string, PatchedFile>();
 
         public PatchScript(RawPatchScript raw)
         {
@@ -23,7 +19,7 @@ namespace SimpleModder
 
             foreach (var patch in raw.Patches)
             {
-                _patches[patch.Key] = patch.Value.Select(Patch.Compile).ToList();
+                _patches[patch.Key] = new PatchedFile(patch.Key, patch.Value);
             }
         }
 
@@ -32,17 +28,9 @@ namespace SimpleModder
             Logger.Log("开始执行补丁【测试模式】");
             foreach (var patchedFile in _patches)
             {
-                Logger.Log($"处理文件：{patchedFile.Key}");
-                var stream = File.Open(Path.Combine(programPath, patchedFile.Key), FileMode.Open, FileAccess.Read);
-                var data = new byte[stream.Length];
-                Trace.Assert(await stream.ReadAsync(data, 0, data.Length) == data.Length);
-                stream.Close();
-
-                foreach (var patch in patchedFile.Value)
-                {
-                    data = patch.RunOn(data);
-                }
+                await patchedFile.Value.DryRunOn(programPath);
             }
+
             Logger.Log("补丁执行完成");
         }
     }
---
+++ SimpleModder/PatchedFile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using SimpleModder.Patches;
+
+namespace SimpleModder
+{
+    public class PatchedFile
+    {
+        private readonly string _filename;
+        private readonly List<Patch> _patches;
+
+        public PatchedFile(string filename, List<RawPatch> raw)
+        {
+            _filename = filename;
+            _patches = raw.Select(Patch.Compile).ToList();
+        }
+
+        public async Task DryRunOn(string programPath)
+        {
+            Logger.Log($"处理文件：{_filename}");
+            var stream = File.Open(Path.Combine(programPath, _filename), FileMode.Open, FileAccess.Read);
+            var data = new byte[stream.Length];
+            Trace.Assert(await stream.ReadAsync(data, 0, data.Length) == data.Length);
+            stream.Close();
+
+            foreach (var patch in _patches)
+            {
+                data = patch.RunOn(data);
+            }
+        }
+    }
+}
